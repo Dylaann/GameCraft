4b3abb576e179a8502b86d12129c40b73e583bec
zjacques <zachary.jacques160@gmail.com>
2019-03-22 16:52:07 +0000

    bgm added and fades to next track at 10 height

--- Gamecraft2019/GameScene.cpp
+++ Gamecraft2019/GameScene.cpp
@@ -6,14 +6,29 @@ GameScene::GameScene()
 
 void GameScene::start()
 {
+	m_bgm1 = m_resources->getSoundResource("BGM1");
+	m_bgm2 = m_resources->getSoundResource("BGM2");
+	//Load the sound effects
+	m_click = Mix_LoadWAV("./ASSETS/SOUNDS/click.wav");
+	Mix_PlayMusic(m_bgm1, -1);
+	m_gameStarted = true;
 }
 
 void GameScene::stop()
 {
+	delete m_bgm1;
+	delete m_bgm2;
+	delete m_click;
 }
 
 void GameScene::update()
 {
+	//m_height++;
+	if (m_height == 10)
+	{
+		Mix_FadeOutMusic(500);
+		Mix_FadeInMusic(m_bgm2, -1, 250);
+	}
 }
 
 void GameScene::draw(SDL_Renderer & renderer)
--- Gamecraft2019/GameScene.h
+++ Gamecraft2019/GameScene.h
@@ -15,10 +15,11 @@ public:
 	void draw(SDL_Renderer& renderer);
 	//void handleInput(InputSystem& input);
 private:
-	bool m_audioCreated;
 	bool m_gameStarted, m_gameOver;
 
-	int m_songIndex;
+	int m_height = 0;
 
-
+	Mix_Music* m_bgm1;
+	Mix_Music* m_bgm2;
+	Mix_Chunk* m_click;
 };

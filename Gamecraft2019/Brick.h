fe64f73a1dd66996c2e4906e467f07329fde2da0
Kevin Andersen <c00225541@itcarlow.ie>
2019-03-22 14:34:14 +0000

    Added Vector2f util class and Brick base class

--- Gamecraft2019/Brick.h
+++ Gamecraft2019/Brick.h
@@ -12,11 +12,16 @@ public:
 
 	void update();
 	void render(SDL_Renderer & renderer);
+
+	Vector2f getPosition() { return m_position; }
+	Vector2f getVelocity() { return m_position; }
+	Vector2f getSize() { return m_size; }
 private:
 	SDL_Texture * m_texture;
 	SDL_Surface * m_surface;
 	Vector2f m_position;
 	Vector2f m_velocity;
+	Vector2f m_size;
 };
 
 #endif

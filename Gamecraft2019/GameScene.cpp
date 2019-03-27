5d87b2f8b75d530d5703e81be6e31a5887188f46
Dylaann <dyl.mrph@gmail.com>
2019-03-27 19:39:06 +0000

    Made producer consumer threads for blocks

--- Gamecraft2019/GameScene.cpp
+++ Gamecraft2019/GameScene.cpp
@@ -1,5 +1,65 @@
 #include "GameScene.h"
 
+
+void producer(GameScene * scene, int n)
+{
+	int count = n;
+
+	while (true) {
+		if (scene->m_q1.size() < count) {
+			std::unique_lock<std::mutex> lck(scene->mx);
+			int k = rand() % 7;
+			Brick * m_temp = new Brick(1, scene->brickTexture, Vector2f(37.5 * k, 76));
+			scene->m_q1.push_front(m_temp);
+			scene->produced++;
+			lck.unlock();
+			scene->cv.notify_one();
+		}
+		else if (scene->m_q2.size() < count) {
+			std::unique_lock<std::mutex> lck(scene->mx);
+			int k = rand() % 7;
+			Brick * m_temp = new Brick(2, scene->brickTexture, Vector2f(37.5 * k, 76));
+			scene->m_q2.push_front(m_temp);
+			scene->produced++;
+			lck.unlock();
+			scene->cv.notify_one();
+		}
+		else if (scene->m_q3.size() < count) {
+			std::unique_lock<std::mutex> lck(scene->mx);
+			int k = rand() % 7;
+			Brick * m_temp = new Brick(3, scene->brickTexture, Vector2f(37.5 * k, 76));
+			scene->m_q3.push_front(m_temp);
+			scene->produced++;
+			lck.unlock();
+			scene->cv.notify_one();
+		}
+	}
+}
+
+void consumer(GameScene * scene)
+{
+	while (true) {
+		std::unique_lock<std::mutex> lck(scene->mx);
+		scene->cv.wait(lck, [scene]() { 
+			return scene->m_currentbrick == nullptr; });
+		if (scene->m_currentBrickLen == 1) {
+			scene->m_currentbrick = scene->m_q1.back();
+			scene->m_q1.pop_back();
+		}
+		else if (scene->m_currentBrickLen == 2) {
+			scene->m_currentbrick = scene->m_q2.back();
+			scene->m_q2.pop_back();
+		}
+		else if (scene->m_currentBrickLen == 3) {
+			scene->m_currentbrick = scene->m_q3.back();
+			scene->m_q3.pop_back();
+		}
+		scene->consumed++;
+		lck.unlock();
+	}
+}
+
+
 GameScene::GameScene()
 {
 
@@ -15,6 +75,15 @@ void GameScene::start()
 	const char *path = "ASSETS\\FONTS\\arial.ttf";
 	Arial = TTF_OpenFont(path, 50);
 
+	m_currentbrick = nullptr;
+	brickTexture = m_resources->getImageResource("blue");
+
+	if (!threadsMade) {
+		producerThread = std::move(std::thread(&producer, this, 1));
+		consumerThread = std::move(std::thread(&consumer, this));
+		threadsMade = true;
+	}
+
 	m_stack = 1;
 	m_currentBrickLen = 3;
 	m_score = 0;
@@ -28,9 +97,10 @@ void GameScene::start()
 	m_message_rect.h = 70;
 
 	m_grid = new Grid(m_resources);
-	brickTexture = m_resources->getImageResource("blue");
-	int k = rand() % 7;
-	m_currentbrick = new Brick(3, brickTexture, Vector2f(37.5 * k, 76));
+
+
+	//int k = rand() % 7;
+	//m_currentbrick = new Brick(3, brickTexture, Vector2f(37.5 * k, 76));
 	//m_testbrick->setDropped(false);
 	m_bgm1 = m_resources->getSoundResource("BGM1");
 	m_bgm2 = m_resources->getSoundResource("BGM2");
@@ -61,6 +131,9 @@ void GameScene::stop()
 
 void GameScene::update()
 {
+	if (m_currentbrick == nullptr) {
+		std::cout << "";
+	}
 	m_currentbrick->update(m_stack, m_score);
 	//m_height++;
 	m_generator.genBricks();
@@ -114,6 +187,7 @@ void GameScene::update()
 		m_currentBrickLen = m_currentbrick->getBlockSize();
 
 		if (m_currentBrickLen < 1) {
+			
 			std::cout << "Enter Name: " << std::endl;
 			std::string name;
 			std::cin >> name;
@@ -122,8 +196,9 @@ void GameScene::update()
 		}
 	
 
-		int k = rand() % 7;
-		m_currentbrick = new Brick(m_currentBrickLen, brickTexture, Vector2f(37.5 * k, 76));
+		//int k = rand() % 7;
+		m_currentbrick = nullptr;
+		cv.notify_one();
 	}
 	else {
 		for (int i = 0; i < m_currentbrick->getBlockSize(); i++) {
@@ -147,8 +222,10 @@ void GameScene::draw(SDL_Renderer & renderer)
 	SDL_FreeSurface(surfaceMessage);
 
 	m_grid->render(&renderer);
-	if (!m_currentbrick->getDone()) {
-		m_currentbrick->render(renderer);
+	if (m_currentbrick != nullptr) {
+		if (!m_currentbrick->getDone()) {
+			m_currentbrick->render(renderer);
+		}
 	}
 
 	SDL_RenderCopy(&renderer, m_message, NULL, &m_message_rect);
--- Gamecraft2019/GameScene.h
+++ Gamecraft2019/GameScene.h
@@ -7,6 +7,9 @@
 #include <vector>
 #include <SDL_ttf.h>
 #include "LeaderBoard/LeaderBoard.h"
+#include <queue>
+#include <mutex>
+#include <condition_variable>
 
 class GameScene : public Scene
 {
@@ -19,14 +22,30 @@ public:
 	bool getCheckGame() { return false; }
 
 	void draw(SDL_Renderer& renderer);
+
+	int produced = 0;
+	int consumed = 0;
+	std::mutex mx;
+	std::condition_variable cv;
+	std::deque<Brick*> m_q1;
+	std::deque<Brick*> m_q2;
+	std::deque<Brick*> m_q3;
+	bool used = false;
+
+	Brick * m_currentbrick = nullptr;
+	SDL_Texture * brickTexture;
+	int m_currentBrickLen = 3;
+
+	std::thread consumerThread;
+	std::thread producerThread;
+
+
 	//void handleInput(InputSystem& input);
 private:
 	BrickGen m_generator;
 
 	bool m_gameStarted, m_gameOver;
 
-	Brick * m_currentbrick;
-	SDL_Texture * brickTexture;
 	int m_songIndex;
 	int m_height = 0;
 	Grid * m_grid;
@@ -35,7 +54,6 @@ private:
 	Mix_Music* m_bgm2;
 	Mix_Chunk* m_click;
 	int m_stack = 1;
-	int m_currentBrickLen = 3;
 	int m_score = 0;
 	std::vector<Brick*> m_bricks;
 
@@ -46,4 +64,5 @@ private:
 	SDL_Color m_scoreCol = { 255, 255, 255, 255 };
 
 	LeaderBoard * m_leaderboard;
+	bool threadsMade = false;
 };
